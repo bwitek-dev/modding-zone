2ae66ff2cde486f388940ea958dda1f70d0ad8b5
spytko <kwanderleute@gmail.com>
2025-09-19 11:33:09 +0200

    init project

---
+++ src/main/java/org/spytko/moddingzone/ModdingZoneApplication.java
@@ -0,0 +1,13 @@
+package org.spytko.moddingzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModdingZoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModdingZoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/spytko/moddingzone/ModdingZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spytko.moddingzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModdingZoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
